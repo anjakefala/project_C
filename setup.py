f46b823af8a31199925564cc6fff67041e5d10bc
anjakefala <anja.kefala@gmail.com>
2021-01-15 10:47:53 -0800

    Fix syntax

--- setup.py
+++ setup.py
@@ -143,7 +143,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['git+https://github.com/anjakefala/project_B', 'git+https://github.com/anjakefala/project_A'],  # Optional
+    install_requires=['project_B @ git+https://github.com/anjakefala/project_B', 'project_A @ git+https://github.com/anjakefala/project_A'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
